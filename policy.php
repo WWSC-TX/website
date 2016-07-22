212693facaccd052c276b432b72f49bc8c7bd68d
westonwater <westonwater@gmail.com>
2016-07-22 17:45:49 -0500

    Enable website_config for policy page

--- policy.php
+++ policy.php
@@ -7,17 +7,21 @@ $smarty->assign('page_name_location', 'images/titles/policy.png');
 $smarty->assign('page_name_alt', 'Policy Information');
 
 // Weston WSC Policy
-$smarty->assign('tariff_title', 'Weston Water Supply Corporation Tariff');
-$smarty->assign('tariff_file', 'Tariff_01-01-2014.pdf');
-
-$smarty->assign('bylaws_title', 'Weston Water Supply Corporation By-Laws');
-$smarty->assign('bylaws_file', 'bylaws_r2013-01.pdf');
-
-$smarty->assign('drought_title', 'Drought Contingency Plan');
-$smarty->assign('drought_file', 'drought_contingency_plan_10-27-2014.pdf');
-
-$smarty->assign('privacy_title', 'Privacy Policy');
-$smarty->assign('privacy_file', 'privacy.pdf');
+$wc_links = explode("\n", file_get_contents('website_config/links'));
+$policy_links = array();
+$c = 0;
+while (true) {
+	$line = array_shift($wc_links);
+	if (strpos($line, '--') === 0) {
+		$c++;
+		continue;
+	}
+	if ($c > 2) break;
+	if ($c < 2) continue;
+	$line = explode('|', $line);
+	$policy_links[$line[0]] = trim($line[1]);
+}
+$smarty->assign('policy_links', $policy_links);
 
 // Display page
 $smarty->display('policy.tpl');
--- templates/policy.tpl
+++ templates/policy.tpl
@@ -3,13 +3,9 @@
 <div class="colmask twocol">
 	<div class="colleft">
 		<div class="col1">	<!-- Right column start -->
-		<h2><a href="downloads/{$tariff_file}">{$tariff_title}</a></h2>
-		
-		<h2><a href="downloads/{$bylaws_file}">{$bylaws_title}</a></h2>
-		
-		<h2><a href="downloads/{$drought_file}">{$drought_title}</a></h2>
-		
-		<h2><a href="downloads/{$privacy_file}">{$privacy_title}</a></h2>
+{foreach $policy_links as $title=>$file}
+			<h2><a href="downloads/{$file}">{$title}</a></h2>
+{/foreach}
 		</div>							<!-- Right column end -->
 		<div class="col2">	<!-- Left column start -->
 		{include file="news.tpl"}
