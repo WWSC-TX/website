25418f177f62ad89a408f3cbe4b51f38ff6feb3e
westonwater <westonwater@gmail.com>
2016-07-22 17:34:43 -0500

    Read center column front page from website_config

--- news_index.php
+++ news_index.php
@@ -2,17 +2,21 @@
 if(!defined('COMMON_INCLUDED'))
 	exit(1);
 
+$wc_links = explode("\n", file_get_contents('website_config/links'));
+$news = array_shift($wc_links);
+$news_index_links = array();
+while(true) {
+	$line = array_shift($wc_links);
+	if (strpos($line, '--') === 0) break;
+	$line = explode('|', $line);
+	if (strpos($line[1], 'http') === 0) {
+		$news_index_links['external'][$line[0]] = trim($line[1]);
+	} else {
+		$news_index_links['file'][$line[0]] = trim($line[1]);
+	}
+}
 $smarty->assign('news', 'Weston Water News');
-$smarty->assign('text', array(
-	'Rate Increase from North Texas Groundwater Conservation District' => 'notification_of_rate_increase.pdf',
-	'Consumer Confidence Report' => 'CCR 2014.pdf'
-));
-$smarty->assign('links', array(
-	'Water IQ: Know your Water' => 'http://www.wateriq.org/',
-	'Water, Use it Wisely' => 'http://www.wateruseitwisely.com/index.php',
-	'Texas Water Conservation Association' => 'http://www.twca.org/',
-	'Texas Water Development Board' => 'http://www.twdb.texas.gov/',
-	'North Texas Groundwater Conservation District' => 'http://northtexasgcd.org/',
-	'Texas Commission on Environmental Quality (TCEQ)' => 'http://www.tceq.state.tx.us/'
-))
+$smarty->assign('news_text', $news);
+$smarty->assign('text', $news_index_links['file']);
+$smarty->assign('links', $news_index_links['external']);
 ?>
